eb4e213ee7e4f8425d91d0b92a153cefeafaa6c6
s1923846 <s1923846@ed.ac.uk>
2021-11-14 16:32:28 +0000

    movement

---
+++ movement.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import Float64MultiArray, Float64
+
+
+def movement_publisher():
+    # Defines publisher and subscriber
+    # initialize the node named
+    rospy.init_node('movement_publisher', anonymous=True)
+    rate = rospy.Rate(1)  # 50hz
+
+    robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
+    robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
+    robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
+
+    t0 = rospy.get_time()
+    while not rospy.is_shutdown():
+        cur_time = np.array([rospy.get_time()]) - t0
+        # y_d = float(6 + np.absolute(1.5* np.sin(cur_time * np.pi/100)))
+        joint2 = Float64()
+        joint3 = Float64()
+        joint4 = Float64()
+        joint2.data = (np.pi / 2) * np.sin(cur_time * np.pi / 15)
+        joint3.data = (np.pi / 2) * np.sin(cur_time * np.pi / 20)
+        joint4.data = (np.pi / 2) * np.sin(cur_time * np.pi / 18)
+
+        robot_joint2_pub.publish(joint2)
+        robot_joint3_pub.publish(joint3)
+        robot_joint4_pub.publish(joint4)
+
+        rate.sleep()
+
+
+# run the code if the node is called
+if __name__ == '__main__':
+    try:
+        movement_publisher()
+    except rospy.ROSInterruptException:
+        pass
