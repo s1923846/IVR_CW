8e88a8bbdb16392abe7be8bda06a51fe92ffc24d
LanceBao0313 <seriously0313@gmail.com>
2021-11-17 18:38:45 +0000

    before andy's attempt for 2.1

--- vision_1.py
+++ vision_1.py
@@ -18,8 +18,6 @@ class image_converter:
         # initialize the node named image_processing
         rospy.init_node('image_processing', anonymous=True)
 
-        # initialize a publisher to send images from camera1 to a topic named image_topic1
-        self.image_pub1 = rospy.Publisher("image_topic1", Image, queue_size=10)
         # initialize a publisher to send joint angle to a topic named joint_angle_2
         self.joint2_pub = rospy.Publisher("joint_angle_2", Float64, queue_size=10)
         # initialize a publisher to send joint angle to a topic named joint_angle_2
@@ -45,12 +43,12 @@ class image_converter:
         self.cv_image2 = np.array([])
 
         # Hardcode the y coordinate of green and yellow sphere because they don't move.
-        self.green_center = np.array([395, 405, 542])
-        self.yellow_center = np.array([401, 402, 430])
-        self.pixel2meter_1 = 0.0375
-        self.pixel2meter_2 = 0.035086369433113516
+        self.green_center = np.array([399, 400, 543])
+        self.yellow_center = np.array([399, 399, 440])
+        #self.pixel2meter_1 = 0.0375
+        #self.pixel2meter_2 = 0.035086369433113516
         #self.yellow_center_2 = np.array([401, 430])
-# 0.03802281
+        # 0.03802281
 
 
 
@@ -62,7 +60,7 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        print(self.pixel2meter(self.cv_image1, True))
+        #print(self.pixel2meter(self.cv_image1, True))
         # Uncomment if you want to save the image
         # cv2.imwrite('image_copy.png', cv_image)
 
@@ -84,21 +82,16 @@ class image_converter:
         im = cv2.imshow('camera1 and camera2', image)
         cv2.waitKey(1)
         #print(self.pixel2meter(self.cv_image2, False))
-        self.joint_angle_2 = Float64()
-        self.joint_angle_2.data = self.calculate_joint_angle()
-        self.joint_angle_3 = Float64()
-        self.joint_angle_3.data = self.calculate_joint_angle()
-        self.joint_angle_4 = Float64()
-        self.joint_angle_4.data = self.calculate_joint_angle()
-
+        joint2 = Float64()
+        joint3 = Float64()
+        joint4 = Float64()
+        joint2.data, joint3.data, joint4.data = self.joint_angles()
         # Publish the results
         try:
-            self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
-            self.joint2_pub.publish(self.joint_angle_2)
-            self.joint3_pub.publish(self.joint_angle_3)
-            self.joint4_pub.publish(self.joint_angle_4)
-
-
+            #self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
+            self.joint2_pub.publish(joint2)
+            self.joint3_pub.publish(joint3)
+            self.joint4_pub.publish(joint4)
         except CvBridgeError as e:
             print(e)
     # In this method you can focus on detecting the centre of the red circle
@@ -125,7 +118,7 @@ class image_converter:
                 return np.array([cx, 0, cy])
 
     # Detecting the centre of the blue circle
-    def detect_blue(self, image,is_image1):
+    def detect_blue(self, image, is_image1):
         mask = cv2.inRange(image, (100, 0, 0), (255, 0, 0))
         kernel = np.ones((5, 5), np.uint8)
         mask = cv2.dilate(mask, kernel, iterations=3)
@@ -170,7 +163,7 @@ class image_converter:
     # Calculate the conversion from pixel to meter
     def pixel2meter(self, image,is_image1):
         # Obtain the centre of each coloured blob
-        circle1Pos = self.detect_red(image, is_image1)
+        circle1Pos = self.detect_green(image)#, is_image1)
         circle2Pos = self.detect_yellow(image)
         # find the distance between two circles
 
@@ -179,48 +172,79 @@ class image_converter:
         #a = np.linalg.norm(np.array(temp1[0],temp1[2])-temp2)
         #print("length: " + str(a))
 
-        print('pos1' + str(circle1Pos))
-        print('pos2' + str(circle2Pos))
-        dist = circle1Pos[2] - circle2Pos[1]
+        #print('pos1' + str(circle1Pos))
+        #print('pos2' + str(circle2Pos))
+        dist = circle1Pos[1] - circle2Pos[1]
         return 10 / dist
-
-    # Calculate the relevant joint angles from the image
-    def detect_joint_angles(self, image):
-        a = self.pixel2meter(image)
-        # Obtain the centre of each coloured blob
-        center = a * self.detect_yellow(image)
-        circle1Pos = a * self.detect_blue(image)
-        circle2Pos = a * self.detect_green(image)
-        circle3Pos = a * self.detect_red(image)
-        # Solve using trigonometry
-        ja1 = np.arctan2(center[0] - circle1Pos[0], center[1] - circle1Pos[1])
-        ja2 = np.arctan2(circle1Pos[0] - circle2Pos[0], circle1Pos[1] - circle2Pos[1]) - ja1
-        ja3 = np.arctan2(circle2Pos[0] - circle3Pos[0], circle2Pos[1] - circle3Pos[1]) - ja2 - ja1
-        return np.array([ja1, ja2, ja3])
 
     def calculate_pos(self, color):
         pos_1 = np.array([])
         pos_2 = np.array([])
-        if(color == 'red'):
-            pos_1 = self.detect_red(self.cv_image1)
-            pos_2 = self.detect_red(self.cv_image2)
-        elif(color == 'blue'):
-            pos_1 = self.detect_blue(self.cv_image1)
-            pos_2 = self.detect_blue(self.cv_image2)
-        x = (pos_2[0] - self.green_center[0]) * self.pixel2meter_2
-        y = (pos_1[1] - self.green_center[1]) * self.pixel2meter_1
-        z_1 = (pos_1[2] - self.green_center[2]) * self.pixel2meter_1
-        z_2 = (pos_2[2] - self.green_center[2]) * self.pixel2meter_2
-        z = (z_1 + z_2) / 2
+        if color == 'red':
+            pos_1 = self.detect_red(self.cv_image1, True)
+            pos_2 = self.detect_red(self.cv_image2, False)
+
+        elif color == 'blue':
+            pos_1 = self.detect_blue(self.cv_image1, True)
+            pos_2 = self.detect_blue(self.cv_image2, False)
+            above_yellow1 = (pos_1[1] - self.yellow_center[2]) < 0
+            above_yellow2 = (pos_2[1] - self.yellow_center[2]) < 0
+            if not (above_yellow1 & above_yellow2):
+                print(color)
+                print(np.array([(pos_2[0] - self.green_center[0]), (pos_1[1] - self.green_center[1]), 104]))
+                return np.array([(pos_2[0] - self.green_center[0]), (pos_1[1] - self.green_center[1]), 104])
+        x = (pos_2[0] - self.green_center[0])
+        y = (pos_1[1] - self.green_center[1])
+        z_1 = (pos_1[2] - self.green_center[2])
+        z_2 = (pos_2[2] - self.green_center[2])
+        z = (-z_1 - z_2) / 2
+        print(color)
+        print(np.array([x, y, z]))
         return np.array([x, y, z])
 
+    def all_coordinates(self):
+        green = np.array([0, 0, 0])
+        yellow = np.array([0, 0, 102])
+        blue = self.calculate_pos('blue')
+        #print(blue)
+        red = self.calculate_pos('red')
+        return [green, yellow, blue, red]
+
+    def calc_angle(self, v1, v2):
+        d = np.dot(v1, v2)
+        l1 = np.linalg.norm(v1)
+        l2 = np.linalg.norm(v2)
+        angle = np.arccos(d / (l1 * l2))
+        return angle
+
+    def joint_angles(self):
+        green, yellow, blue, red = self.all_coordinates()
+        yellow2blue = blue - yellow
+
+        #yellow2blue_xz = np.array([yellow2blue[0], yellow2blue[2]])
+        blue2red = red - blue
+        #print(yellow2blue_xz)
+        joint2 = self.calc_angle(yellow2blue, yellow)
+        if joint2 > np.pi / 2:
+            joint2 = np.pi - joint2
+        if joint2 < (- np.pi / 2):
+            joint2 = - np.pi + joint2
+        #print(joint2)
+        joint3 = self.calc_angle(yellow2blue, yellow)
+        if joint3 > np.pi / 2:
+            joint3 = np.pi - joint3
+        if joint3 < (- np.pi / 2):
+            joint3 = - np.pi + joint3
+
+        joint4 = self.calc_angle(yellow2blue, blue2red)
+        if joint4 > np.pi / 2:
+            joint4 = np.pi - joint4
+        if joint4 < (- np.pi / 2):
+            joint4 = np.pi + joint4
+        return [joint2, joint3, joint4]
 
 
 
-
-
-    def calculate_joint_angle(self):
-        return 1.0;
 # call the class
 def main(args):
     ic = image_converter()
